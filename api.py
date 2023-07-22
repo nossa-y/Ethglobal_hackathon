f951367a33852bc04a5ebd01bd179dea2445313d
Nossa <samuel.iyamu@hotmail.fr>
2023-07-22 10:50:24 +0200

    V1 de l'optimisation de chemin

--- api.py
+++ api.py
@@ -1,7 +1,25 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 
-class transaction(BaseModel):
+class poids(BaseModel):
+    btc : float
+    eth : float
+    ltc : float
+    xrp : float
 
+class poids_mod(BaseModel):
+    btc : float = None
+    eth : float = None
+    ltc : float = None
+    xrp : float = None
 
-app 
+app = FastAPI()
+
+@app.get("/poids/")
+def get_transac(blockchain : poids):
+    return blockchain
+
+"""@app.post("/transac/{id}")
+def edit_transac(id, blockchain : poids_mod):
+    if poids_mod.btc is not None:
+        blockchain[id].btc = poids"""
--- main.py
+++ main.py
@@ -25,19 +25,52 @@ def chemins(fichier):
                 s += line.value()[i]
                 chemin.append(line.key()[i])
                 p += 1
-        chemins.append(chemin,s) #à verifier
+        L = [chemin,s]
+chemins.extend(L) #attention chemin sera de la forme [[[btc,eth,xrp],4],[[btc,xrp],2]]
+
 un_dico = {
-    eth
+    "btc":{
+        "btc" : 0.5,
+        "eth" : 1,
+        "ltc" : 2,
+        "xrp" : 3,
+    },
+    "eth":{
+        "btc" : 0.5,
+        "eth" : 1,
+        "ltc" : 2,
+        "xrp" : 3
+    },
+    "ltc":{
+        "btc" : 0.5,
+        "eth" : 1,
+        "ltc" : 1,
+        "xrp" : 3
+    },
+    "xrp":{
+        "btc" : 0.5,
+        "eth" : 1,
+        "ltc" : 2,
+        "xrp" : 1
+    }
+
 }
 
 #algo en fonction de ce que l'utilisateur veut 
-@app.get
-def get_plus_court_chemin(user_blockchain, wanted_blockchain):
+def plus_court_chemin(user_blockchain, wanted_blockchain): #blockchain de depaart, blockchain d'arrivée
     best_chemins = []
     for chemin in chemins:
         if user_blockchain == chemin.key()[0] and wanted_blockchain in chemin:
-            best_chemins.append(chemin)
-            prendre le chemin tq s est le plus petit
+            best_chemins.append(chemin) #on a tout les blockchain tq chemin_de_depart = blockchain de l'utilisateur, blockchain d'arrivée = blockchain voulue
+    L = []
+    for i in best_chemins:
+        L.append(i[-1]) #on obtient la liste de tous les poids
+    m = min(L)
+    q = 0
+    while best_chemins[q][-1] != m: #recherche du chemin de poids minimal
+        q += 1
+    soluce = best_chemins[q]
+    return soluce
 
 
 
